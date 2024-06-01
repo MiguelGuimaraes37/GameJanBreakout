e778b84524932c8bab2d883dd7418bdbd714e244
deli <deli@Ubuntu1804.deli.virtualbox.org>
2024-06-01 16:46:17 +0100

    Add Breakout phrase test

--- Breakout/src/Main.java
+++ Breakout/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Breakout phrase test");
+        
         System.out.println("Hello world!");
     }
 }
